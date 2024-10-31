5026427d66aaaf66946e38ea5610620c81fff1f3
arnav-jain25 <62302885+arnav-jain25@users.noreply.github.com>
2024-10-31 12:31:25 -0700

    Delete javascript directory

--- javascript/navbar.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 30) { // Change this value to adjust when the color changes
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
